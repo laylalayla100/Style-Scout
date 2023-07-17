c14c39f126da20621be89bdd64acdf962dc60c84
laylalayla100 <awesomeljh@icloud.com>
2023-07-17 17:49:22 -0400

    Initial Commit

---
+++ Style Scout/Style_ScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Style_ScoutApp.swift
+//  Style Scout
+//
+//  Created by layla hunter on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Style_ScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
